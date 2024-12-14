9fc87f6835d4ef0c7b6983ec3796dc46a4f58385
antish <101444378+antish24@users.noreply.github.com>
2024-12-14 13:06:09 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 8800,
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI:"mongodb+srv://Ish:ycDx5HriscAeF61w@miniapp.61vbg.mongodb.net/?retryWrites=true&w=majority&appName=Miniapp",
   JWT_SECRET:process.env.JWTSECRET,
   expiresIn:process.env.EXPIREDIN , // Token expiration time
   // smsUser:process.env.SMSUSER , // Token expiration time
